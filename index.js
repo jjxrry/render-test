8d7d07a455a02659b47ea32709e12c14c8289a3a
jjxrry <114961129+jjxrry@users.noreply.github.com>
2023-02-16 10:46:53 -0800

    test render deploy

--- index.js
+++ index.js
@@ -13,18 +13,12 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('dist'))
 
-
-let notes = [
-]
+let notes = []
  
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
@@ -32,24 +26,25 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
     
     if (body.content === undefined) {
         return response.status(400).json({ error: 'content missing' })
-      }
+    }
     
-      const note = new Note({
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-      })
+    })
     
-      note.save().then(savedNote => {
+    note.save().then(savedNote => {
         response.json(savedNote)
-      })
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
     .then(note => {
         if (note) {
@@ -58,20 +53,48 @@ app.get('/api/notes/:id', (request, response) => {
             response.status(404).end()
         }
     })
-    .catch(error => {
-        console.log(error);
-        response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
+app.put(`/api/notes/:id`, (request, response, next) => {
+    const {content, important} = request.body
+
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        {content, important}, 
+        {new: true, runValidators: true, context: `query`}
+    )
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === `CastError`) {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === `ValidationError`) {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+app.use(errorHandler) //any error handler needs to be last loaded middleware
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
